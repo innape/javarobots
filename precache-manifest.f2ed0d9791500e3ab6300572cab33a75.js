4a375ab7220671cfd3d4e96a98410363fea5cab9
innape <innapetrova87@hotmail.com>
2020-05-12 22:18:52 +0100

    Updates

---
+++ precache-manifest.f2ed0d9791500e3ab6300572cab33a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a4e4051387f0815d25aba6265e3864",
+    "url": "/javarobots/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/javarobots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "07028860a1dac2705b26",
+    "url": "/javarobots/static/css/main.5982afae.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/javarobots/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javarobots/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07028860a1dac2705b26",
+    "url": "/javarobots/static/js/main.a236ffa9.chunk.js"
+  },
+  {
+    "revision": "81aa5715450b6f6816d9",
+    "url": "/javarobots/static/js/runtime-main.446410cb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/javarobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
