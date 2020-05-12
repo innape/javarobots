b556bafb73551b1fe2e4ad1226df37f8bd748d42
innape <innapetrova87@hotmail.com>
2020-05-12 21:34:30 +0100

    uploading JavaFriends

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 3,
+    name: 'Nick Mayes',
+    username: 'Boo',
+    email: 'nick.m@gmai.com'
+  },
+  {
+    id: 'leeman',
+    name: 'Lee Mansbridge',
+    username: 'The Boos',
+    email: 'lee.m@gmai.com'
+  },
+  {
+    id: 1,
+    name: 'Mike Harris',
+    username: 'MyCarIs',
+    email: 'mike.h@gmai.com'
+  },
+  {
+    id: 4,
+    name: 'Georgie The Cat',
+    username: 'Ginger',
+    email: 'unknown@gmai.com'
+  },
+  {
+    id: 9,
+    name: 'Holly',
+    username: '',
+    email: 'holly.c@gmai.com'
+  },
+  {
+    id: 'matt',
+    name: 'Mattew',
+    username: '',
+    email: 'mattew.s@gmai.com'
+  },
+  {
+    id: 'natw',
+    name: 'Nat W',
+    username: '',
+    email: 'nat.w@gmai.com'
+  },
+  {
+    id: 'natbrown',
+    name: 'Nat B',
+    username: '',
+    email: 'nat.b@gmai.com'
+  },
+  {
+    id: 'mahmud',
+    name: 'Mahmud',
+    username: '',
+    email: 'mahmud@gmai.com'
+  },
+  {
+    id: 'johnhe',
+    name: 'John',
+    username: '',
+    email: 'John.h@gmai.com'
+  }
+];
